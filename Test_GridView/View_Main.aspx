eb91a5f0a31084a9fe154ee9b800dea2cce0cabc
saihe <saihe.kyon@gmail.com>
2017-12-06 10:00:59 +0900

    test

---
+++ Test_GridView/View_Main.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="View_Main.aspx.cs" Inherits="Test_GridView.View_Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Test GridView</title>
+</head>
+<body>
+	<asp:Panel ID="panelMain" runat="server">
+		<asp:GridView ID="gridViewEmployee" runat="server"></asp:GridView>
+	</asp:Panel>
+	<asp:Panel ID="panelSub" runat="server">
+		<asp:GridView ID="gridViewSelectedRecordInfo" runat="server"></asp:GridView>
+	</asp:Panel>
+</body>
+</html>
---
+++ Test_GridView/View_Main.aspx.designer.cs
@@ -0,0 +1,51 @@
+﻿//------------------------------------------------------------------------------
+// <自動生成>
+//     このコードはツールによって生成されました。
+//
+//     このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+//     コードが再生成されるときに損失したりします。 
+// </自動生成>
+//------------------------------------------------------------------------------
+
+namespace Test_GridView {
+    
+    
+    public partial class View_Main {
+        
+        /// <summary>
+        /// panelMain コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel panelMain;
+        
+        /// <summary>
+        /// gridViewEmployee コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gridViewEmployee;
+        
+        /// <summary>
+        /// panelSub コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel panelSub;
+        
+        /// <summary>
+        /// gridViewSelectedRecordInfo コントロール。
+        /// </summary>
+        /// <remarks>
+        /// 自動生成されたフィールド。
+        /// 変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gridViewSelectedRecordInfo;
+    }
+}
